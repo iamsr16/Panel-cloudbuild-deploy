3c9986ddc025aa4aad7a62a46c373133d1de8acf
iamsr16 <83810443+iamsr16@users.noreply.github.com>
2023-02-02 19:10:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,6 +17,12 @@ provider "google" {
   project = var.project
 }
 
+resource "google_cloud_run_service" "service" {
+  depends_on = [
+    google_project_service.enabled_service["run.googleapis.com"]
+  ]
+  name     = var.namespace
+  location = var.region
 
 
 # Enable services
